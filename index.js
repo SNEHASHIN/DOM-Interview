06698c30af4e6b7ed3b05738c7fe91abbd802237
sneha shinde <Sneha06032000@gmail.com>
2022-08-14 12:05:36 +0530

    dom interview

---
+++ index.js
@@ -0,0 +1,41 @@
+const createBtn = document.getElementById("task")
+const hidden = document.getElementById("hide")
+const red1 = document.getElementsByClassName("red1")[0]
+const yellow1 = document.getElementsByClassName("yellow1")[0]
+const blue1 = document.getElementsByClassName("blue1")[0]
+const black1 = document.getElementsByClassName("black1")[0]
+const textarea = document.getElementsByClassName("textarea")[0]
+const done = document.getElementsByClassName("btn2")[0]
+createBtn.addEventListener("click", () => {
+    hidden.style.display="block";
+})
+
+red1.addEventListener("click", ()=>{
+    textarea.style.backgroundColor="red"
+})
+yellow1.addEventListener("click", ()=>{
+    textarea.style.backgroundColor="yellow"
+})
+blue1.addEventListener("click", ()=>{
+    textarea.style.backgroundColor="blue"
+})
+black1.addEventListener("click", ()=>{
+    textarea.style.backgroundColor="black"
+})
+done.addEventListener("click", () =>{
+    hidden.style.display="none";
+})
+
+closebtn.addEventListener("click", () => {
+    e.target.parentElement.remove();
+})
+
+
+
+
+
+
+    
+    
+
+    
